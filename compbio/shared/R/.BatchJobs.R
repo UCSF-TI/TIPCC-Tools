7aa60c0432dea566a47aa4c6091b440f0e699a78
Bengtsson <hb@biostat.ucsf.edu>
2014-10-05 14:08:27 -0700

    Added setup scripts and R BatchJobs config files

---
+++ compbio/shared/R/.BatchJobs.R
@@ -0,0 +1,26 @@
+######################################################################
+# A BatchJobs cluster configuration R script
+#
+# Author: Henrik Bengtsson
+#
+# References:
+# [1] https://github.com/tudo-r/BatchJobs/wiki/Configuration
+######################################################################
+
+cluster.functions <- makeClusterFunctionsTorque("/home/henrik/shared/R/.BatchJobs,torque.tmpl")
+
+# PATCH: http://goo.gl/YuYqoi
+cluster.functions$listJobs = function(conf, reg) {
+  runOSCommandLinux("qselect", "-u $USER -s EHQRTW")$output
+}
+
+# To avoid file system locks, cf. [1]
+staged.queries <- TRUE
+
+######################################################################
+# HISTORY:
+# 2014-09-10
+# o Now staging queries.
+# 2013-12-18
+# o Shared on the cluster.
+######################################################################
