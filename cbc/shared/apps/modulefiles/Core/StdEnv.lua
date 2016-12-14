91d5efacb88f81d8b60ab937bc3d6d718b765113
hb <hb@aroma-project.org>
2016-12-13 17:50:58 -0800

    MODULES: Add system default modules and one for enabling Spack software

---
+++ cbc/shared/apps/modulefiles/Core/StdEnv.lua
@@ -0,0 +1,7 @@
+-- Default compiler tools
+load("gcc/4.9.2")
+
+-- Spack installed packages
+-- NOTE: Don't enable by default until we know it works
+-- load("spack-gcc-4.9.2")
+
---
+++ cbc/shared/apps/modulefiles/Core/spack-gcc-4.9.2.lua
@@ -0,0 +1,10 @@
+load("spack")
+
+local spack_root = os.getenv("SPACK_ROOT")
+
+-- ADHOC: Hard coded for now /HB 2016-12-13
+local spack_arch = "linux-rhel6-x86_64"  -- On head node == $(spack arch)
+local compiler = pathJoin("gcc", "4.9.2")
+
+local home = pathJoin(spack_root, "share/spack/lmod", spack_arch, compiler)
+prepend_path("MODULEPATH", home)
