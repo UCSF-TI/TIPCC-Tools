7cdb6760d358c78ec7e086f564b36966d52ed8ee
hb <hb@aroma-project.org>
2016-12-16 13:22:56 -0800

    CLEANUP: Moved shell settings to .bashrc.d/shell.sh [#10]

--- cbc/shared/.bashrc.d/shell,aliases.sh
+++
@@ -1,5 +0,0 @@
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Convenient ways of listing files
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-alias ll="ls -l"
-alias lll="ls -la"
---
+++ cbc/shared/.bashrc.d/shell.sh
@@ -0,0 +1,21 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Aliases
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+alias ll="ls -l"
+alias lll="ls -la"
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Default editor
+# Needed by for instance 'svn commit' etc
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if test "${EDITOR}" = ""; then
+  export EDITOR="emacs -nw"
+fi
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Misc
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Never use /usr/libexec/openssh/gnome-ssh-askpass
+unset SSH_ASKPASS
