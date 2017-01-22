adfe2c1ccd3e623d9c7f1e42da48fd23588fad86
hb <hb@aroma-project.org>
2017-01-21 20:49:41 -0800

    Lmod: Add cbc-tools replicating the ad hoc CBC Tools setup

---
+++ cbc/shared/apps/modulefiles/Linux/cbc-tools/0.1.0.lua
@@ -0,0 +1,45 @@
+help([[
+CBC Tools
+]])
+
+local name = "cbc-tools"
+local version = "0.1.0"
+whatis("Version: " .. version)
+whatis("Keywords: UCSF, cluster, utilities")
+whatis("URL: https://github.com/UCSF-TI/TIPCC/wiki/Software-shared-by-CBC")
+whatis("Description: CBC Tools setup via modules.")
+
+load("cbc-bin")
+
+-- load("git")
+-- load("curl")
+-- load("wget")
+-- load("tar")
+-- load("zlib")
+-- load("emacs")
+
+load("bedops")
+load("bedtools2")
+load("blast")
+load("bowtie2")
+load("bwa")
+load("control-freec")
+load("cufflinks")
+load("fastqc")
+load("genetorrent")
+load("htseq")
+load("sratoolkit")
+load("samtools")
+load("bcftools")
+load("tophat")
+load("vcftools")
+load("wordspy")
+load("igvtools")
+load("igv")
+load("htslib")
+load("s3cmd")
+-- load("valgrind") ## Because it adds /usr/bin/ to PATH
+load("jdk")
+load("matlab")
+load("r")
+load("udocker")
