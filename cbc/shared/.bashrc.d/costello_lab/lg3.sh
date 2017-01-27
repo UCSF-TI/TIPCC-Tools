b244aa5394f64d411f1093afcd428be02dee62a4
hb <hb@aroma-project.org>
2017-01-26 21:36:08 -0800

    LG3 as .bashrc.d/

---
+++ cbc/shared/.bashrc.d/costello_lab/lg3.sh
@@ -0,0 +1,4 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# LG3 Exome Pipeline
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+export LG3_HOME=/home/henrik/projects/CostelloJ_2014-LG3-Exome-Pipeline/pbs
