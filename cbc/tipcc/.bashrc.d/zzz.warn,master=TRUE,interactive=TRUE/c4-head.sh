6645c817712420c88b665bcaa165ee6147e54555
hb <hb@aroma-project.org>
2017-02-14 16:27:19 -0800

    ROBUSTNESS: Script will no also check that c4-head exists before calling it

--- cbc/tipcc/.bashrc.d/zzz.warn,master=TRUE,interactive=TRUE/c4-head.sh
+++ cbc/tipcc/.bashrc.d/zzz.warn,master=TRUE,interactive=TRUE/c4-head.sh
@@ -1,10 +1,9 @@
 ## Already done?
 if [[ $STARTUP_DONE == *"c4-head"* ]]; then return; fi
 
-## Running in interactive mode?
-if [[ "$PS1" ]]; then
-    ## ... on the head node? (c4 head only works there)
-    if test "$HOSTNAME" == "cclc01.som.ucsf.edu"; then
+## Running in interactive mode and on the head node?
+if [[ "$PS1" && "$HOSTNAME" == "cclc01.som.ucsf.edu" ]]; then
+    if [[ -x /home/shared/cbc/bin/c4-head ]]; then
         bfr=$(/home/shared/cbc/bin/c4-head --user ${USER})
         if [[ -n "${bfr}" ]]; then
             tput setaf 3 2> /dev/null ## yellow
@@ -17,6 +16,6 @@ if [[ "$PS1" ]]; then
             >&2 echo
             tput sgr0 2> /dev/null    ## reset
         fi
-	STARTUP_DONE="${STARTUP_DONE}:c4-head"
     fi
+    STARTUP_DONE="${STARTUP_DONE}:c4-head"
 fi
