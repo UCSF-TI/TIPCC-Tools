f3c9c8fab4b73c65f1a1207746f75fa0c99c2f3b
hb <hb@aroma-project.org>
2017-04-12 16:29:44 -0700

    Added Pandoc to CBC-shared tools.  It was installed using Anaconda and the directory was copied over. Hopefully that'll work.

--- cbc/shared/.bashrc.d/software,utils.sh
+++ cbc/shared/.bashrc.d/software,utils.sh
@@ -2,3 +2,8 @@
 # SOFTWARE: System tools
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 prependPath ${SHARED_SOFTWARE}/htop-latest
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# SOFTWARE: Text tools
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+prependPath ${SHARED_SOFTWARE}/pandoc-latest/bin
