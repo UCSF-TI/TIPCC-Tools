ea9d2e228285023e047ee4e091e6ac970d5a2c70
hb <hb@aroma-project.org>
2017-04-27 18:38:17 -0700

    CLEANUP: setup of python is now cleaner and easier to tweak

--- cbc/shared/.bashrc.d/software,0.python.sh
+++ cbc/shared/.bashrc.d/software,0.python.sh
@@ -2,7 +2,9 @@
 # SOFTWARE: Python
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##prependPath /opt/local/bin # Python 2.6.5
-prependPath ${SHARED_SOFTWARE}/Python-2.7.4/bin
+if [[ $(using_lmod) -eq 0 ]]; then
+  module_load Python
+fi
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -11,34 +13,7 @@ prependPath ${SHARED_SOFTWARE}/Python-2.7.4/bin
 # FIXME: We should probably remove Anaconda, because it causes
 #        many issues.
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-## Prepare such that only users with ~/.cbc.anaconda will
-## add the CBC-shared installation to their paths
-if [[ -f "~/.cbc.anaconda" ]]; then
-   prependPath /home/shared/cbc/software_cbc/anaconda/bin
-else
-   prependPath /home/shared/cbc/software_cbc/anaconda/bin
+if [[ $(using_lmod) -eq 0 ]]; then
+  prependPath /home/shared/cbc/software_cbc/anaconda/bin
+  . /home/shared/cbc/bin/pythonpath_update
 fi
-
-PYTHON_VERSION=$(python -c 'import sys; print("%s.%s" % (sys.version_info[0], sys.version_info[1]));')
-PYTHON_HOME=$(which python)
-
-if test "${PYTHONPATH_SYSTEM}" == ""; then
-  export PYTHONPATH_SYSTEM=${PYTHONPATH}
-fi
-export PYTHONPATH=${PYTHONPATH_SYSTEM}
-
-# Add user-specific Python binaries
-dir=${HOME}/.local/bin
-if ! test -d "${dir}"; then
-  mkdir -p "${dir}"
-fi
-export PYTHONPATH=${dir}:${PYTHONPATH}
-prependPath ${dir}
-
-# Add site-wide Python packages
-if test "${PYTHON_VERSION}" == "2.6"; then
-  PYTHON_SITE=/opt/local/lib/python2.6/site-packages
-else
-  PYTHON_SITE=$(dirname $(dirname $PYTHON_HOME))/lib/python${PYTHON_VERSION}
-fi
-export PYTHONPATH=${PYTHONPATH}:${PYTHON_SITE}
