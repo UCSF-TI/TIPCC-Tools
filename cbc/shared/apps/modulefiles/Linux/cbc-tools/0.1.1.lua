54a5f51648e72106c57731a43fd35a273b5bfe9e
hb <hb@aroma-project.org>
2017-04-27 18:39:36 -0700

    MODULES: Update cbc-tools to include anaconda and python

---
+++ cbc/shared/apps/modulefiles/Linux/cbc-tools/0.1.1.lua
@@ -0,0 +1,53 @@
+help([[
+CBC Tools
+]])
+
+local name = "cbc-tools"
+local version = "0.1.1"
+whatis("Version: " .. version)
+whatis("Keywords: UCSF, cluster, utilities")
+whatis("URL: https://github.com/UCSF-TI/TIPCC/wiki/Software-shared-by-CBC")
+whatis("Description: CBC Tools setup via modules.")
+
+load("cbc-bin")
+
+-- load("pcre")
+
+-- load("git")
+-- load("curl")
+-- load("pcre")
+-- load("wget")
+-- load("tar")
+-- load("zlib")
+-- load("emacs")
+
+load("python")
+load("anaconda")
+load("anaconda-python")
+
+load("bedops")
+load("bedtools2")
+load("blast")
+load("bowtie2")
+load("bwa")
+load("control-freec")
+load("cufflinks")
+load("fastqc")
+load("genetorrent")
+load("htseq")
+load("sratoolkit")
+load("samtools")
+load("bcftools")
+load("tophat")
+load("vcftools")
+load("wordspy")
+load("igvtools")
+load("igv")
+load("htslib")
+load("s3cmd")
+-- load("valgrind") ## Because it adds /usr/bin/ to PATH
+load("jdk")
+load("matlab")
+load("r")
+prepend_path("R_LIBS_SITE", "/home/shared/cbc/R/site-library/%p-library/%v")
+load("udocker")
