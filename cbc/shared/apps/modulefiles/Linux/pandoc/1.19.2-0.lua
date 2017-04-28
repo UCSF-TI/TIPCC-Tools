2dbbbd234c99fea4586377ee235127727081c923
hb <hb@aroma-project.org>
2017-04-27 20:41:36 -0700

    MODULES: adding pandoc

---
+++ cbc/shared/apps/modulefiles/Linux/pandoc/1.19.2-0.lua
@@ -0,0 +1,18 @@
+help([[
+Pandoc - a universal document converter
+]])
+
+local name = "pandoc"
+local version = "1.19.2-0"
+whatis("Version: " .. version)
+whatis("Keywords: documentation, conversion")
+whatis("URL: http://pandoc.org/")
+whatis("Description: Pandoc is a Haskell library for converting from one markup format to another, and a command-line tool that uses this library.")
+
+-- Local variables
+local cbc_shared = "/home/shared/cbc"
+local cbc_software = cbc_shared .. "/software"
+
+local home = cbc_software .. "/" .. name .. "-" .. version
+prepend_path("PATH", home .. "/bin")
+
