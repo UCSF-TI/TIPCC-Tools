d2df7c259d67f5e32be91fbbca6a17b6e827c59a
hb <hb@aroma-project.org>
2017-04-28 16:57:14 -0700

    MODULES: Add build script and other version for Python

---
+++ cbc/shared/apps/modulefiles/Linux/python/2.7.3.lua
@@ -0,0 +1,19 @@
+help([[
+The Python Programming Language
+]])
+
+local name = "Python"
+local version = "2.7.3"
+whatis("Version: " .. version)
+whatis("Keywords: Programming")
+whatis("URL: https://www.python.org/")
+whatis("Description: The Python programming language")
+
+-- Local variables
+local cbc_shared = "/home/shared/cbc"
+local cbc_software = cbc_shared .. "/software"
+
+local home = cbc_software .. "/" .. name .. "-" .. version
+prepend_path("PATH", home .. "/bin")
+prepend_path("LD_LIBRARY_PATH", home .. "/lib")
+prepend_path("PYTHONPATH", home .. "/lib/python2.7")
