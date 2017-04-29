1b4667d0898b51adeb563db3f6cda17ed65935ba
hb <hb@aroma-project.org>
2017-04-28 21:46:45 -0700

    Updated IGV and IGVTools

---
+++ cbc/shared/apps/modulefiles/Linux/igvtools/2.3.91.lua
@@ -0,0 +1,16 @@
+help([[
+IGVTools
+]])
+
+local name = "IGVTools"
+local version = "2.3.91"
+whatis("Version: " .. version)
+whatis("Keywords: ")
+whatis("URL: https://software.broadinstitute.org/software/igv/igvtools")
+whatis("Description: The igvtools utility provides a set of tools for pre-processing data files.")
+
+-- Local variables
+local cbc_shared = "/home/shared/cbc"
+local cbc_software = cbc_shared .. "/software"
+
+prepend_path("PATH", cbc_software .. "/" .. name .. "-" .. version)
