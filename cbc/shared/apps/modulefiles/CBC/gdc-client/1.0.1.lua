9ce5490aaf1438269b4fa22bce4d7fa1d5a3efe4
hb <hb@aroma-project.org>
2017-06-20 13:42:07 -0700

    MODULE: gdc-client - some tweaks

--- cbc/shared/apps/modulefiles/CBC/gdc-client/1.0.1.lua
+++ cbc/shared/apps/modulefiles/CBC/gdc-client/1.0.1.lua
@@ -2,8 +2,8 @@ help([[
 GDC Data Transfer Tool
 ]])
 
-local name = "gdc-client"
-local version = "1.0.1"
+local name = myModuleName()
+local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: data, transfer")
 whatis("URL: https://gdc.cancer.gov/access-data/gdc-data-transfer-tool")
@@ -14,3 +14,6 @@ local cbc_shared = "/home/shared/cbc"
 local cbc_software = cbc_shared .. "/software"
 
 prepend_path("PATH", cbc_software .. "/" .. name .. "-" .. version .. "/bin")
+
+load("python/2.7.10")
+unload("anaconda-python")
