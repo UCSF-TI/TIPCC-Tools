1d12ec7bb10e7ae90c756722d44a7bb9cfe5d877
hb <hb@aroma-project.org>
2017-06-20 12:20:02 -0700

    BUG FIX: Syntax error in 'snvmix2'

--- cbc/shared/apps/modulefiles/CBC/snvmix2/0.11.8-r3.lua
+++ cbc/shared/apps/modulefiles/CBC/snvmix2/0.11.8-r3.lua
@@ -1,17 +1,16 @@
 help([[
-SNVMix2
+SNVMix2 - Detecting Single Nucleotide Variants from Next Generation Sequencing Data
 ]])
 
-local name = "snvmix2"
-local version = "0.11.8-r3"
+local name = myModuleName()
+local version = myModuleVersion()
 whatis("Version: " .. version)
-whatis("Keywords: ")
+whatis("Keywords: sequencing, genomics")
 whatis("URL: http://compbio.bccrc.ca/software/snvmix/")
 whatis("Description: SNVMix is designed to detect single nucleotide variants from next generation sequencing data. SNVMix is a post-alignment tool.")
 
 -- Local variables
 local cbc_shared = "/home/shared/cbc"
 local cbc_software = cbc_shared .. "/software"
-
-local path cbc_software .. "/" .. name .. "-" .. version
+local path = cbc_software .. "/" .. name .. "-" .. version
 prepend_path("PATH", path)
