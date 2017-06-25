304af38bd003e517642153f0415557253a5e12ef
hb <hb@aroma-project.org>
2017-06-25 12:35:53 -0700

    Add (TMP|VARTMP|SCRATCH)_USER environment variables

--- cbc/tipcc/.bashrc.d/000.tipcc,folders.sh
+++ cbc/tipcc/.bashrc.d/000.tipcc,folders.sh
@@ -1,21 +1,38 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Personalized temporary and scratch directory
-# (e.g. cleaner /tmp/ directory on cluster)
+# /tmp/ folders
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-export TMPDIR=/tmp/$USER
-if ! test -d "$TMPDIR"; then
-  mkdir -p "$TMPDIR"
+export TMP_USER="/tmp/$USER"
+if [[ ! -d "$TMP_USER" ]]; then
+  mkdir -p "$TMP_USER"
 fi
 
-export VARTMPDIR=/var/tmp/$USER
-if ! test -d "$VARTMPDIR"; then
-  mkdir -p "$VARTMPDIR"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# /var/tmp/ folders
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+export VARTMP_USER="/var/tmp/$USER"
+if [[ ! -d "$VARTMP_USER" ]]; then
+  mkdir -p "$VARTMP_USER"
 fi
 
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# /scratch/ folders
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Create /scratch/$USER/ (only possible on compute nodes)
-if test -d "/scratch"; then
-  export SCRATCHDIR=/scratch/$USER
-  if ! test -d "$SCRATCHDIR"; then
-    mkdir -p "$SCRATCHDIR"
+if [[ -d "/scratch" ]]; then
+  export SCRATCH_USER="/scratch/$USER"
+  if [[ ! -d "$SCRATCH_USER" ]]; then
+    mkdir -p "$SCRATCH_USER"
   fi
+fi
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Default folders
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+export TMPDIR="$TMP_USER"
+export VARTMPDIR="$VARTMP_USER"
+if [[ -d "/scratch" ]]; then
+  export SCRATCHDIR="$SCRATCH_USER"
 fi
