a5048562fbee5a23fbdb9ec5581e9c3000a83af7
hb <hb@aroma-project.org>
2017-07-03 14:34:35 -0700

    Alert user if they may expose credentials in 'ps'

---
+++ cbc/tipcc/.bashrc.d/zzz.warn,interactive=TRUE/credentials-exposed.sh
@@ -0,0 +1,21 @@
+## Already done?
+if [[ $STARTUP_DONE == *"warn-credentials"* ]]; then return; fi
+
+## Running in interactive mode?
+if [[ "$PS1" ]]; then
+    if [[ -x /home/shared/cbc/tipcc/bin/tipcc-ps ]]; then
+        bfr=$(/home/shared/cbc/tipcc/bin/tipcc-ps --user ${USER} | grep -E "lftp .*-u[ ]+[^,]+,[^ ]+")
+        if [[ -n "${bfr}" ]]; then
+            tput setaf 1 2> /dev/null ## red
+            >&2 echo
+            >&2 echo "*************************************************************************"
+            >&2 echo "** ALERT: You are exposing your credentials to all users on the cluster! "
+            >&2 echo "**        See for instance 'ps aux | grep \$USER'                        "
+            >&2 echo "*************************************************************************"
+            >&2 printf '%s\n' "${bfr[@]}"
+            >&2 echo
+            tput sgr0 2> /dev/null    ## reset
+        fi
+    fi
+#    STARTUP_DONE="${STARTUP_DONE}:warn-credentials"
+fi
