fc6d87e8036c50d038da95901c2705baeb1d36b8
hb <hb@aroma-project.org>
2017-07-06 13:28:17 -0700

    Updated path to tipcc/bin/tipcc-master

--- cbc/tipcc/.bashrc.d/zzz.warn,interactive=TRUE/master=TRUE/tipcc-master.sh
+++ cbc/tipcc/.bashrc.d/zzz.warn,interactive=TRUE/master=TRUE/tipcc-master.sh
@@ -3,8 +3,8 @@ if [[ $STARTUP_DONE == *"c4-head"* ]]; then return; fi
 
 ## Running in interactive mode and on the head node?
 if [[ "$PS1" && "$HOSTNAME" == "cclc01.som.ucsf.edu" ]]; then
-    if [[ -x /home/shared/cbc/bin/c4-head ]]; then
-        bfr=$(/home/shared/cbc/bin/c4-head --user ${USER})
+    if [[ -x /home/shared/cbc/tipcc/bin/tipcc-master ]]; then
+        bfr=$(/home/shared/cbc/tipcc/bin/tipcc-master --user ${USER})
         if [[ -n "${bfr}" ]]; then
             tput setaf 3 2> /dev/null ## yellow
             >&2 echo
