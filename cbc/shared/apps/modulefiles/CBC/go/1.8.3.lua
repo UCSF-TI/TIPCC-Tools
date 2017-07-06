6eb6e889ef275fa72b88878abdd0ca770b27c8bf
hb <hb@aroma-project.org>
2017-07-06 12:14:12 -0700

    MODULES: Add 'go'

---
+++ cbc/shared/apps/modulefiles/CBC/go/1.8.3.lua
@@ -0,0 +1,22 @@
+help([[
+The Go Programming Language
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: programming, language, Go")
+whatis("URL: https://golang.org//")
+whatis("Description: Go is a cross-platform open-source programming language that makes it easy to build simple, reliable, and efficient software.")
+
+-- Local variables
+local cbc_shared = "/home/shared/cbc"
+local cbc_software = cbc_shared .. "/software"
+
+local goroot = cbc_software .. "/" .. name .. "-" .. version
+setenv("GOROOT", goroot)
+prepend_path("PATH", goroot .. "/bin")
+
+local gopath = os.getenv("HOME") .. "/go"
+setenv("GOPATH", gopath)
+prepend_path("PATH", gopath .. "/bin")
