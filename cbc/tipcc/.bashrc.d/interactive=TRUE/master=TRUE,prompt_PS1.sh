1cf8fed18790ba882952d72fc20ada531f738d72
hb <hb@aroma-project.org>
2017-07-12 21:26:38 -0700

    Mention PROMPT_COMMAND in comments

--- cbc/tipcc/.bashrc.d/interactive=TRUE/master=TRUE,prompt_PS1.sh
+++ cbc/tipcc/.bashrc.d/interactive=TRUE/master=TRUE,prompt_PS1.sh
@@ -1,16 +1,17 @@
 ## Description: Highlight, in command-line prompt, that user is on master
 ##
 ## Note: In addition to PS1 there's also PS2 (for continuation)
+## as well as PROMPT_COMMAND.
 ##
 ## See also: A neat PS1 generator (http://bashrcgenerator.com/)
 if [[ "$PS1" && "$HOSTNAME" == "cclc01.som.ucsf.edu" ]]; then
     case $TERM in
         xterm*)
-## OR?	    export PS1='\e[1;31m\]** ON MASTER **\[\e[m\] [\u@\h \W]\$ '
+## OR?	    export PS1='\[\e[1;31m\]** ON MASTER **\[\e[m\] [\u@\h \W]\$ '
             export PS1='[\u@\[\e[1;31m\]\h\[\e[m\] \W]\$ '
             ;;
         screen)
-## OR?	    export PS1='\e[1;31m\]** ON MASTER **\[\e[m\] [\u@\h \W]\$ '
+## OR?	    export PS1='\[\e[1;31m\]** ON MASTER **\[\e[m\] [\u@\h \W]\$ '
             export PS1='[\u@\[\e[1;31m\]\h\[\e[m\] \W]\$ '
             ;;
         *)
