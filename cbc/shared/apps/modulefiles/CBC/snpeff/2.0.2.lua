811f35c5fbc474ae6c523d98f5780b3b75e64e72
hb <hb@aroma-project.org>
2017-07-12 20:00:09 -0700

    MODULES: fix 'snpeff' version format

--- cbc/shared/apps/modulefiles/CBC/snpeff/2.0.2.lua
+++ cbc/shared/apps/modulefiles/CBC/snpeff/2.0.2.lua
@@ -1,19 +1,19 @@
 help([[
-SnpEff
+SnpEff - Genetic Variant Annotation and Effect Prediction Toolbox
 ]])
 
 local name = "snpEff"
-local version = "2_0_2"
+local version = "2.0.2"
 whatis("Version: " .. version)
 whatis("Keywords: high-throughput sequencing")
 whatis("URL: http://snpeff.sourceforge.net/")
-whatis("Description: Genetic variant annotation and effect prediction toolbox.")
+whatis("Description: SnpEff is a variant annotation and effect prediction tool. It annotates and predicts the effects of variants on genes (such as amino acid changes).")
 
 -- Local variables
 local cbc_shared = "/home/shared/cbc"
 local cbc_software = cbc_shared .. "/software"
 
-local path = cbc_software .. "/" .. name .. "_v" .. version
+local path = cbc_software .. "/" .. name .. "-" .. version
 local pathname = path .. "/snpEff.jar"
 
 set_alias("snpEff", "java -jar " .. pathname)
