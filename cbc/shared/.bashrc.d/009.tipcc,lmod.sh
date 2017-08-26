abd8b7a5152afbfa759feea7707a2d9c3b279028
hb <hb@aroma-project.org>
2017-08-25 19:50:35 -0700

    CBC setup: Explicitly set 'tcl' modules; soon to become 'lmod'

--- cbc/shared/.bashrc.d/009.tipcc,lmod.sh
+++ cbc/shared/.bashrc.d/009.tipcc,lmod.sh
@@ -1,3 +1,6 @@
+## The default module framework is 'tcl', but will soon become 'lmod,quiet' /HB 2017-08-25
+MODULE_FRAMEWORK=${MODULE_FRAMEWORK:-tcl}
+
 if [[ $STARTUP_DONE != *"lmod"* ]]; then
     . $SHARED_ROOT/tipcc/.bashrc.d/009.lmod.sh
 fi
