5d71be99ef7197f73fb4812357586adabceb3969
hb <hb@aroma-project.org>
2017-10-24 21:33:11 -0700

    CLEANUP: Deprecated useR and all R-version aliases

--- cbc/shared/.bashrc.d/zzz.non-lmod-stray.sh
+++ cbc/shared/.bashrc.d/zzz.non-lmod-stray.sh
@@ -1,31 +1,17 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Aliases
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-## TODO: Replace these aliases with warnings
-## to use 'module load r/x.y.z instead.
-alias R-2.15.3=${SHARED_SOFTWARE}/R-2.15.3/bin/R
-alias R-3.0.0=${SHARED_SOFTWARE}/R-3.0.0/bin/R
-alias R-3.0.1=${SHARED_SOFTWARE}/R-3.0.1/bin/R
-alias R-3.0.2=${SHARED_SOFTWARE}/R-3.0.2/bin/R
-alias R-3.0.3=${SHARED_SOFTWARE}/R-3.0.3/bin/R
-alias R-3.1.0=${SHARED_SOFTWARE}/R-3.1.0/bin/R
-alias R-3.1.1=${SHARED_SOFTWARE}/R-3.1.1/bin/R
-alias R-3.1.2=${SHARED_SOFTWARE}/R-3.1.2/bin/R
-alias R-3.1.3=${SHARED_SOFTWARE}/R-3.1.3/bin/R
-alias R-3.2.0=${SHARED_SOFTWARE}/R-3.2.0/bin/R
-alias R-3.2.1=${SHARED_SOFTWARE}/R-3.2.1/bin/R
-alias R-3.2.3=${SHARED_SOFTWARE}/R-3.2.3/bin/R
-alias R-3.3.0=${SHARED_SOFTWARE}/R-3.3.0/bin/R
-alias R-3.3.1=${SHARED_SOFTWARE}/R-3.3.1/bin/R
-alias R-3.3.2=${SHARED_SOFTWARE}/R-3.3.2/bin/R
-alias R-3.3.3=${SHARED_SOFTWARE}/R-3.3.3/bin/R
-alias R-latest=${SHARED_SOFTWARE}/R-latest/bin/R
-alias R-patched=${SHARED_SOFTWARE}/R-patched/bin/R
-alias R-devel=${SHARED_SOFTWARE}/R-devel/bin/R
-## TODO: Update useR to give a warning
-## to use 'module load r/x.y.z instead.
-alias useR=". ${SHARED_ROOT}/bin/useR"
+if [[ $(using_lmod) -eq 0 ]]; then
+  for vv in 2.15.3 3.0.0 3.0.1 3.0.2 3.0.3 3.1.0 3.1.1 3.1.2 3.1.3 3.2.0 3.2.1 3.2.3 3.3.0 3.3.1 3.3.2 3.3.3 latest patched devel; do
+    eval "alias R-${vv}='${SHARED_SOFTWARE}/R-${vv}/bin/R'"
+  done
+  alias useR=". ${SHARED_ROOT}/bin/useR"
+else
+  for vv in 2.15.3 3.0.0 3.0.1 3.0.2 3.0.3 3.1.0 3.1.1 3.1.2 3.1.3 3.2.0 3.2.1 3.2.3 3.3.0 3.3.1 3.3.2 3.3.3 latest patched devel; do
+    eval "alias R-${vv}='tput setaf 3; echo "WARNING: alias R-${vv} is deprecated - instead use module load r/${vv}"; tput sgr0; ${SHARED_SOFTWARE}/R-${vv}/bin/R'"
+  done
+  alias useR=". ${SHARED_ROOT}/bin/useR"
+fi
 
 # Mirror website
 alias wmirror="wget --no-host-directories --recursive --no-parent --reject='index.html*'"
