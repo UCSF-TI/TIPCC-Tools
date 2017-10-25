46359f111e431e48b00e19e9f928d5f06e63742a
hb <hb@aroma-project.org>
2017-10-25 16:25:52 -0700

    CLEANUP: ~/.local/bin is already added to PATH by the TIPCC-wide startup

--- cbc/shared/.bashrc.d/001.user-bin.sh
+++
@@ -1,7 +0,0 @@
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# User installed software paths
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-## 'pip install --user <foo>' installs into ~/.local/bin/
-if [[ -d "${HOME}/.local/bin" ]]; then
-  prependPath ${HOME}/.local/bin
-fi
