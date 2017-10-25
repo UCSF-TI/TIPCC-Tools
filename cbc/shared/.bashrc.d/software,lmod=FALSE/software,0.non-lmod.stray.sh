f332cca37f7cc9bcec7756ab3689bdf715c0eb47
hb <hb@aroma-project.org>
2017-10-24 19:22:33 -0700

    CLEANUP: Taking a chance and wont set LUA_PATH and LUA_CPATH for lmod=TRUE

---
+++ cbc/shared/.bashrc.d/software,lmod=FALSE/software,0.non-lmod.stray.sh
@@ -0,0 +1,26 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Lua, LuaRocks and Lmod
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if [[ $(using_lmod) -eq 0 ]]; then
+    ## module load lua
+    prependPath ${SHARED_SOFTWARE}/lua-latest/bin
+    
+    ## module load luarocks
+    prependPath ${SHARED_SOFTWARE}/luarocks-latest/bin
+
+    ## UPDATE: This used to be done also for lmod=TRUE, but for
+    ## simplicity, lets see if we can ignore that. /HB 2017-10-24
+    ## TODO: Add this to the 'luarocks' module
+    ## Find also Lua modules installed via luarocks install --local
+    ver=$(luarocks config --lua-ver)   
+    export LUA_PATH="${HOME}/.luarocks/share/lua/5.3/?.lua;${HOME}/.luarocks/share/lua/5.3/?/init.lua;/home/shared/cbc/software_cbc/luarocks-2.4.2/share/lua/5.3/?.lua;/home/shared/cbc/software_cbc/luarocks-2.4.2/share/lua/5.3/?/init.lua;/usr/local/share/lua/5.3/?.lua;/usr/local/share/lua/5.3/?/init.lua;/usr/local/lib/lua/5.3/?.lua;/usr/local/lib/lua/5.3/?/init.lua;./?.lua;./?/init.lua"
+    export LUA_CPATH="${HOME}/.luarocks/lib/lua/5.3/?.so;/home/shared/cbc/software_cbc/luarocks-2.4.2/lib/lua/5.3/?.so;/usr/local/lib/lua/5.3/?.so;/usr/local/lib/lua/5.3/loadall.so;./?.so"
+fi
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Ruby (and other tools)
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if [[ $(using_lmod) -eq 0 ]]; then
+    prependPath ${SHARED_SOFTWARE}_cbc/bin
+fi
