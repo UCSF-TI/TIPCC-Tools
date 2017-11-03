52586ee815f158d1d9ba963b239a8a305433ff9f
hb <hb@aroma-project.org>
2017-11-03 09:40:13 -0700

    File permissions: fix_permissions_to_user_only() -> fix_permissions()

--- cbc/tipcc/.bashrc.d/zzz.warn,interactive=TRUE/master=TRUE/privacy-file-permissions.sh
+++ cbc/tipcc/.bashrc.d/zzz.warn,interactive=TRUE/master=TRUE/privacy-file-permissions.sh
@@ -1,26 +1,35 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# SECURITY: Assert file permissions
-# - - - - - - -_- - - - - - - - - - - - - - - - - - - - - - - - - -
-## TODO: For SSH login (to cluster and nodes), make sure the $HOME
-##       folder don't share write permissions with group or others.
-##       /HB & JJ /2017-10-27
+# SSH
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## All users (6 affected)
+## RULE: Home directory must not be writable by anyone but the user in
+##       order for SSH to work.
+## TEST: ls -la /home/ | grep -E "^d....w....$"
+## fix_permissions ${HOME} d....-..-. go-w
 
 ## All users (5 affected)
-## TEST: ls -la /home/*/.ssh | grep -E " [.]$" | grep -vE "[-][-][-][-]"
+## RULE: ~/.ssh/ must not be accessible by any others user.
 ## DISCUSS: Is/should it be ok to share .ssh/ with group?
-#TODO# fix_permissions_to_user_only ${HOME}/.ssh
+## TEST: ls -la /home/*/.ssh | grep -E " [.]$" | grep -vE "[-][-][-][-]"
+#TODO# fix_permissions ${HOME}/.ssh d...------ go-rwx
 
 ## All users (5 affected)
-## TEST: ls -la /home/*/.ssh/authorized_keys | grep -vE "[-][-][-]"
+## RULE: ~/.ssh/authorized_keys must not be accessible by any others user.
 ## DISCUSS: Is/should it be ok to share .ssh/authorized_keys with group?
-#TODO# fix_permissions_to_user_only ${HOME}/.ssh/authorized_keys
+## TEST: ls -la /home/*/.ssh/authorized_keys | grep -vE "[-][-][-]"
+#TODO# fix_permissions ${HOME}/.ssh/authorized_keys -...------ go-rwx
 
-#TODO# fix_permissions_to_user_only ${HOME}/.ssh/id_dsa
-#TODO# fix_permissions_to_user_only ${HOME}/.ssh/id_rsa
+#TODO# fix_permissions ${HOME}/.ssh/id_dsa -...------ go-rwx
+#TODO# fix_permissions ${HOME}/.ssh/id_rsa -...------ go-rwx
 
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## Files that may contain user's authentication credentials
+## and should therefore not be accessible by any other user
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ## 3 users (1 affected)
 ## TEST: ls -la /home/*/.netrc | grep -vE "[-][-][-]"
-#TODO# fix_permissions_to_user_only ${HOME}/.netrc
+#TODO# fix_permissions ${HOME}/.netrc -...------ go-rwx
 
 ## 0 users
 fix_permissions ${HOME}/.pgpass -...------ go-rwx
