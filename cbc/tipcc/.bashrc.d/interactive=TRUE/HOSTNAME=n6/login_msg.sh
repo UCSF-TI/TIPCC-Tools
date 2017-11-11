7630835f3438b507999bff86e35e688a3d397fc4
hb <hb@aroma-project.org>
2017-11-10 16:23:19 -0800

    Clarify temporary n6 message

--- cbc/tipcc/.bashrc.d/interactive=TRUE/HOSTNAME=n6/login_msg.sh
+++ cbc/tipcc/.bashrc.d/interactive=TRUE/HOSTNAME=n6/login_msg.sh
@@ -2,8 +2,8 @@ if [[ "$PS1" && $HOSTNAME == n6 && ! -d /scratch ]]; then
     tput setaf 3 2> /dev/null ## yellow
     >&2 echo
     >&2 echo "*************************************************************************"
-    >&2 echo "* NOTE: /scratch is currently unavailable on node n6."
-    >&2 echo "*       We are working on replacing it. /2017-11-07"
+    >&2 echo "* NOTE: /scratch is currently unavailable on node n6. We are working on     "
+    >&2 echo "*       fixing it. When done, we might have to reboot again. /2017-11-07 "
     >&2 echo "*************************************************************************"
     true
     >&2 echo
