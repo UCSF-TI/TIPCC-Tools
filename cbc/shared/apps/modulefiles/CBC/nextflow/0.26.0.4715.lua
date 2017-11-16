80ce94d2cee3d1954b755ae51a67e9b28cf5ff48
hb <hb@aroma-project.org>
2017-11-16 12:29:57 -0800

    typo

--- cbc/shared/apps/modulefiles/CBC/nextflow/0.26.0.4715.lua
+++ cbc/shared/apps/modulefiles/CBC/nextflow/0.26.0.4715.lua
@@ -1,5 +1,5 @@
 help([[
-nextflow - Data-driven computational pipelines
+Nextflow - Data-driven computational pipelines
 ]])
 
 local name = myModuleName()
@@ -7,7 +7,7 @@ local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: workflow")
 whatis("URL: https://www.nextflow.io/")
-whatis("Description: extflow enables scalable and reproducible scientific workflows using software containers. It allows the adaptation of pipelines written in the most common scripting languages. Its fluent DSL simplifies the implementation and the deployment of complex parallel and reactive workflows on clouds and clusters.")
+whatis("Description: Nextflow enables scalable and reproducible scientific workflows using software containers. It allows the adaptation of pipelines written in the most common scripting languages. Its fluent DSL simplifies the implementation and the deployment of complex parallel and reactive workflows on clouds and clusters.")
 
 -- Local variables
 local cbc_shared = "/home/shared/cbc"
