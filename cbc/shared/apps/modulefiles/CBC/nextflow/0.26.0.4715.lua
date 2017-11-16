a3953a133997aabf0bb419c79d2ba3679c7b3116
hb <hb@aroma-project.org>
2017-11-16 12:24:53 -0800

    MODULES: Added 'nextflow'

---
+++ cbc/shared/apps/modulefiles/CBC/nextflow/0.26.0.4715.lua
@@ -0,0 +1,17 @@
+help([[
+nextflow - Data-driven computational pipelines
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: workflow")
+whatis("URL: https://www.nextflow.io/")
+whatis("Description: extflow enables scalable and reproducible scientific workflows using software containers. It allows the adaptation of pipelines written in the most common scripting languages. Its fluent DSL simplifies the implementation and the deployment of complex parallel and reactive workflows on clouds and clusters.")
+
+-- Local variables
+local cbc_shared = "/home/shared/cbc"
+local cbc_software = cbc_shared .. "/software_cbc"
+
+load("jdk")  -- Requires Java (>= 8)
+prepend_path("PATH", cbc_software .. "/" .. name .. "-" .. version)
