8a3d962f2ecf4c2395fe496bea332267a2ec7627
hb <hb@aroma-project.org>
2017-11-18 14:22:29 -0800

    Create Personal MODULEPATH folder if missing

--- cbc/shared/apps/modulefiles/repos/Personal.lua
+++ cbc/shared/apps/modulefiles/repos/Personal.lua
@@ -2,8 +2,10 @@ help([[
 Modules in ~/modulefiles/
 ]])
 
-whatis("Description: Personal modules in `~/modulefiles/`.  For instance, module definition file `~/modulefiles/foo/1.0.lua` provides module foo/1.0, which can be loaded using `module load foo/1.0`.")
+local name = myModuleName()
+whatis("Description: Personal modules in `~/modulefiles/` (created if missing).  For instance, module definition file `~/modulefiles/foo/1.0.lua` provides module foo/1.0, which can be loaded using `module load foo/1.0`.  For example of module files, see the many modules under /home/shared/cbc/apps/modulefiles/CBC/.")
 
 local home = os.getenv("HOME")
 local mpath = pathJoin(home, "modulefiles")
+os.execute("mkdir -p " .. mpath)
 prepend_path("MODULEPATH", mpath)
