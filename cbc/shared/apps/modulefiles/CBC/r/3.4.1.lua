1a12901d42d9374d437c46182897c18348142acc
hb <hb@aroma-project.org>
2017-11-30 13:14:31 -0800

    MODULES: Add r/3.4.3. Now pointing all PATHs to /software_cbc/R/ locations

--- cbc/shared/apps/modulefiles/CBC/r/3.4.1.lua
+++ cbc/shared/apps/modulefiles/CBC/r/3.4.1.lua
@@ -11,6 +11,6 @@ whatis("Description: The R programming language. Example: `R --version`.")
 
 -- Local variables
 local cbc_shared = "/home/shared/cbc"
-local path = cbc_shared .. "/software/R-" .. version
+local path = cbc_shared .. "/software_cbc/R/R-3.4.1-20170630"
 prepend_path("PATH", path .. "/bin")
 prepend_path("MANPATH", path .. "/share/man")
