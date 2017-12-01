d6737d56699465e089b8fa5ee9200c7638ab0eed
hb <hb@aroma-project.org>
2017-11-30 18:31:54 -0800

    MODULE: Tweak title for 'CBC'

--- cbc/shared/apps/modulefiles/repos/CBC.lua
+++ cbc/shared/apps/modulefiles/repos/CBC.lua
@@ -1,5 +1,5 @@
 help([[
-Module Repository by CBI (Computational Biology and Informatics - formerly CBC)
+Module Repository by Computational Biology and Informatics (CBI - formerly CBC)
 ]])
 
 local name = myModuleName()
@@ -8,6 +8,6 @@ local mpath = pathJoin(mroot, name)
 
 whatis("Keywords: UCSF, CBI, CBC")
 whatis("URL: http://cbi.ucsf.edu/")
-whatis("Description: Repository of modules shared by the Computational Biology and Informatics (http://cbi.ucsf.edu/) which was named Computational Biology Core (CBC) until 2017. When loading this module, you will get access to a large number of software modules.  When unloading the module, any software modules from this " .. name .. " repository still loaded when you unload this repository module, will remain in your list of loaded modules but will be inactivate (i.e. behave as they never were loaded) until you reload this repository module again.  Example: `module load CBC` and then `module avail`.")
+whatis("Description: Repository of modules shared by Computational Biology and Informatics (http://cbi.ucsf.edu/) which was named Computational Biology Core (CBC) until 2017. When loading this module, you will get access to a large number of software modules.  When unloading the module, any software modules from this " .. name .. " repository still loaded when you unload this repository module, will remain in your list of loaded modules but will be inactivate (i.e. behave as they never were loaded) until you reload this repository module again.  Example: `module load CBC` and then `module avail`.")
 
 prepend_path("MODULEPATH", mpath)
