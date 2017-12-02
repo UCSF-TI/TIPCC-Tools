b2c4030189936beb565964a9b66e9e0bc7c08d3b
hb <hb@aroma-project.org>
2017-12-02 11:09:16 -0800

    MODULES: Fix 'ruby' URL

--- cbc/shared/apps/modulefiles/CBC/ruby/2.2.0p0.lua
+++ cbc/shared/apps/modulefiles/CBC/ruby/2.2.0p0.lua
@@ -6,11 +6,9 @@ local name = myModuleName()
 local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: programming, scripting")
-whatis("URL: https://luarocks.org/")
-whatis("Description: Ruby is a dynamic, reflective, object-oriented, general-purpose programming language. It was designed and developed in the mid-1990s by Yukihiro 'Matz' Matsumoto in Japan.")
+whatis("URL: https://www.ruby-lang.org/en/")
+whatis("Description: Ruby is a dynamic, reflective, object-oriented, general-purpose programming language. It was designed and developed in the mid-1990s by Yukihiro 'Matz' Matsumoto in Japan.  Example: `ruby --version`")
 
--- Local variables
-local cbc_shared = "/home/shared/cbc"
-local cbc_software = cbc_shared .. "/software_cbc"
-
-prepend_path("PATH", cbc_software .. "/bin")
+local path = "/home/shared/cbc/software_cbc"
+local home = path
+prepend_path("PATH", home .. "/bin")
