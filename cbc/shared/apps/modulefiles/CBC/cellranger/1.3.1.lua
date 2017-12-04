ecfa5f2dfb114c0f9aea6b61af7424aca0c76e77
hb <hb@aroma-project.org>
2017-12-04 14:45:23 -0800

    MODULES: Add 'cellranger/1.3.1'

---
+++ cbc/shared/apps/modulefiles/CBC/cellranger/1.3.1.lua
@@ -0,0 +1,18 @@
+help([[
+Cell Ranger: 10x Genomics Pipeline for Single-Cell Data Analysis
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: sequencing, 10x genomics")
+whatis("URL: https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/what-is-cell-ranger")
+whatis("Description: Cell Ranger is a set of analysis pipelines that process Chromium Single Cell 3' RNA-seq output to align reads, generate gene-cell matrices and perform clustering and gene expression analysis.  Example: `cellranger`")
+
+
+load("jdk")
+load("bcl2fastq")
+
+local path = "/home/shared/cbc/software_cbc"
+local home = path .. "/" .. name .. "-" .. version
+prepend_path("PATH",  home)
