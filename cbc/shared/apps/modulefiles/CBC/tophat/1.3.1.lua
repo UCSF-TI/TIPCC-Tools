8c73fdd3ad4c8e45d0703e1ea293e28f20c8737a
hb <hb@aroma-project.org>
2017-12-04 17:20:00 -0800

    MODULES: Pointing 'tophat' module PATHs to software_cbc

--- cbc/shared/apps/modulefiles/CBC/tophat/1.3.1.lua
+++ cbc/shared/apps/modulefiles/CBC/tophat/1.3.1.lua
@@ -1,5 +1,5 @@
 help([[
-TopHat
+TopHat: A spliced read mapper for RNA-Seq
 ]])
 
 local name = myModuleName()
@@ -7,11 +7,8 @@ local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: Programming, Statistics")
 whatis("URL: https://ccb.jhu.edu/software/tophat/index.shtml")
-whatis("Description: TopHat is a fast splice junction mapper for RNA-Seq reads. It aligns RNA-Seq reads to mammalian-sized genomes using the ultra high-throughput short read aligner Bowtie, and then analyzes the mapping results to identify splice junctions between exons.")
+whatis("Description: TopHat is a fast splice junction mapper for RNA-Seq reads. It aligns RNA-Seq reads to mammalian-sized genomes using the ultra high-throughput short read aligner Bowtie, and then analyzes the mapping results to identify splice junctions between exons.  Example: `tophat --version`.")
 
--- Local variables
-local cbc_shared = "/home/shared/cbc"
-local cbc_software = cbc_shared .. "/software"
-
-local home = cbc_software .. "/" .. name .. "-" .. version .. ".Linux_x86_64"
+local path = "/home/shared/cbc/software_cbc"
+local home = path .. "/" .. name .. "-" .. version .. ".Linux_x86_64"
 prepend_path("PATH", home)
