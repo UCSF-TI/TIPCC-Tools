fd762397829a53ec4c38b83ec722f16c4c3e8136
hb <hb@aroma-project.org>
2017-12-06 23:31:22 -0800

    MODULES: Add hisat2 and links

---
+++ cbc/shared/apps/modulefiles/CBC/links/2.14.lua
@@ -0,0 +1,15 @@
+help([[
+Links: A Lynx-like Alternative Character Mode WWW Browser
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: internet, browser")
+whatis("URL: http://links.twibright.com/features.php")
+whatis("Description: Links runs in text mode (mouse optional) on UN*X console, ssh/telnet virtual terminal, vt100 terminal, xterm, and virtually any other text terminal. Mouse is supported for GPM, xterm, and OS/2. Links supports colors on terminal. Example: `links https://www.google.com/`.")
+
+local path = "/home/shared/cbc/software_cbc"
+local home = path .. "/" .. name .. "-" .. version
+prepend_path("PATH", home .. "/bin")
+prepend_path("MANPATH", home .. "/man")
