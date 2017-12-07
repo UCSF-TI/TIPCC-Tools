d10c8f3cbae423fc9da55284561c2cc3f336de2d
hb <hb@aroma-project.org>
2017-12-06 23:33:23 -0800

    fix

--- cbc/shared/apps/modulefiles/CBC/links/2.14.lua
+++ cbc/shared/apps/modulefiles/CBC/links/2.14.lua
@@ -9,6 +9,8 @@ whatis("Keywords: internet, browser")
 whatis("URL: http://links.twibright.com/features.php")
 whatis("Description: Links runs in text mode (mouse optional) on UN*X console, ssh/telnet virtual terminal, vt100 terminal, xterm, and virtually any other text terminal. Mouse is supported for GPM, xterm, and OS/2. Links supports colors on terminal. Example: `links https://www.google.com/`.")
 
+load("cbc-devel")
+
 local path = "/home/shared/cbc/software_cbc"
 local home = path .. "/" .. name .. "-" .. version
 prepend_path("PATH", home .. "/bin")
