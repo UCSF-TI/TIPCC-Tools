5108b1e06bf3f0759a7f3b7edc422584aad382bc
hb <hb@aroma-project.org>
2017-12-11 22:59:04 -0800

    MODULES: Add module shellcheck/0.4.6

---
+++ cbc/shared/apps/modulefiles/CBC/shellcheck/0.4.6.lua
@@ -0,0 +1,14 @@
+help([[
+ShellCheck - A shell script static analysis tool
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: programming, shell, sh, bash, dash, ksh")
+whatis("URL: https://www.shellcheck.net/")
+whatis("Description: ShellCheck finds bugs in your shell scripts.  Example: `shellcheck --version` and `shellcheck -x ~/.bashrc`")
+
+local path = "/home/shared/cbc/software_cbc"
+local home = path .. "/" .. name .. "-" .. version
+prepend_path("PATH", home)
