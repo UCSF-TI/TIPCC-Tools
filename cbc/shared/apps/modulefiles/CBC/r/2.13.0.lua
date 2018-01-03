3d575bf2f1272f0f62372baa9b3ae47b7de0765c
hb <hb@aroma-project.org>
2018-01-03 13:04:55 -0800

    MODULES: Add r/2.13.0

---
+++ cbc/shared/apps/modulefiles/CBC/r/2.13.0.lua
@@ -0,0 +1,16 @@
+help([[
+The R Programming Language
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: Programming, Statistics")
+whatis("URL: https://www.r-project.org/")
+whatis("Description: The R programming language. Example: `R --version`.")
+
+-- Local variables
+local cbc_shared = "/home/shared/cbc"
+local path = cbc_shared .. "/software_cbc/R/R-2.13.0-20150918"
+prepend_path("PATH", path .. "/bin")
+prepend_path("MANPATH", path .. "/share/man")
