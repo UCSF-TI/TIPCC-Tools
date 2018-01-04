a5556cd6fcebe28c3b10335778a274c1bcb73a88
hb <hb@aroma-project.org>
2018-01-04 10:42:07 -0800

    MODULES: Add 'picard' modules for all previously know installations

---
+++ cbc/shared/apps/modulefiles/CBC/picard/1.39.lua
@@ -0,0 +1,16 @@
+help([[
+Picard: Command-line tools for Manipulating High-throughput Sequencing Data and Formats
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: sequencing")
+whatis("URL: http://broadinstitute.github.io/picard/")
+whatis("Description: Picard is a set of command line tools for manipulating high-throughput sequencing (HTS) data and formats such as SAM/BAM/CRAM and VCF.  Example: `java -jar $PICARD_HOME/ViewSam.jar --help`")
+
+load("jdk")
+
+local path = "/opt/local"
+local home = path .. "/" .. name .. "-tools-" .. version
+setenv("PICARD_HOME", home)
---
+++ cbc/shared/apps/modulefiles/CBC/picard/1.51.lua
@@ -0,0 +1,17 @@
+help([[
+Picard: Command-line tools for Manipulating High-throughput Sequencing Data and Formats
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: sequencing")
+whatis("URL: http://broadinstitute.github.io/picard/")
+whatis("Description: Picard is a set of command line tools for manipulating high-throughput sequencing (HTS) data and formats such as SAM/BAM/CRAM and VCF.  Example: `java -jar $PICARD_HOME/ViewSam.jar --help`")
+
+load("jdk")
+
+local path = "/home/jocostello/tools"
+-- SIC!
+local home = path .. "/" .. name .. "-tools-" .. version .. "/" .. name .. "-tools-" .. version
+setenv("PICARD_HOME", home)
