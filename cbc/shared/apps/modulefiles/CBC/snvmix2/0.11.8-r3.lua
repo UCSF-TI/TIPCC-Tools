f92ded03bf627eaac47d5d253c11eaf7b12156bd
hb <hb@aroma-project.org>
2018-01-04 10:06:25 -0800

    MODULES: Now 'snvmix2' module points directly to installation path

--- cbc/shared/apps/modulefiles/CBC/snvmix2/0.11.8-r3.lua
+++ cbc/shared/apps/modulefiles/CBC/snvmix2/0.11.8-r3.lua
@@ -1,16 +1,15 @@
 help([[
-SNVMix2 - Detecting Single Nucleotide Variants from Next Generation Sequencing Data
+SNVMix2: Detecting Single Nucleotide Variants from Next Generation Sequencing Data
 ]])
 
 local name = myModuleName()
 local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: sequencing, genomics")
-whatis("URL: http://compbio.bccrc.ca/software/snvmix/")
-whatis("Description: SNVMix is designed to detect single nucleotide variants from next generation sequencing data. SNVMix is a post-alignment tool.")
+whatis("URL: http://shahlab.ca/projects/snvmix/")
+whatis("Description: SNVMix is designed to detect single nucleotide variants from next generation sequencing data. SNVMix is a post-alignment tool. Example: `SNVMix2 -h`.")
 
--- Local variables
-local cbc_shared = "/home/shared/cbc"
-local cbc_software = cbc_shared .. "/software"
-local path = cbc_software .. "/" .. name .. "-" .. version
-prepend_path("PATH", path)
+local path = "/opt/local"
+name = "SNVMix2"
+local home = path .. "/" .. name .. "-" .. version
+prepend_path("PATH", home)
