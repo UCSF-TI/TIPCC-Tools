0f4b448112dc46c3730dcbb64f71a5b836736c87
hb <hb@aroma-project.org>
2018-01-04 09:43:03 -0800

    MODULES: Add 'cufflinks/2.2.1'

---
+++ cbc/shared/apps/modulefiles/CBC/cufflinks/2.2.1.lua
@@ -0,0 +1,14 @@
+help([[
+Cufflinks: Transcriptome Assembly and Differential Expression Analysis for RNA-Seq
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: sequencing")
+whatis("URL: http://cole-trapnell-lab.github.io/cufflinks/")
+whatis("Description: Cufflinks assembles transcripts, estimates their abundances, and tests for differential expression and regulation in RNA-Seq samples. It accepts aligned RNA-Seq reads and assembles the alignments into a parsimonious set of transcripts. Cufflinks then estimates the relative abundances of these transcripts based on how many reads support each one, taking into account biases in library preparation protocols. Example: `cufflinks`.")
+
+local path = "/home/shared/cbc/software_cbc"
+local home = path .. "/" .. name .. "-" .. version .. ".Linux_x86_64"
+prepend_path("PATH", home)
