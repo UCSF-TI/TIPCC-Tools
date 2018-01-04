ec90de62b6cc9b804ec586ba2c5aa1573d24b3ee
hb <hb@aroma-project.org>
2018-01-03 22:38:32 -0800

    MODULES: Now 'gdc-client' module points directly to system installation folder

--- cbc/shared/apps/modulefiles/CBC/gdc-client/1.0.1.lua
+++ cbc/shared/apps/modulefiles/CBC/gdc-client/1.0.1.lua
@@ -1,5 +1,5 @@
 help([[
-GDC Data Transfer Tool
+GDC Data Transfer Tool: NCI Genome Data Commons Data Transfer Tools
 ]])
 
 local name = myModuleName()
@@ -7,13 +7,11 @@ local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: data, transfer")
 whatis("URL: https://gdc.cancer.gov/access-data/gdc-data-transfer-tool")
-whatis("Description: The GDC provides a standard client-based mechanism in support of high performance data downloads and submission.")
-
--- Local variables
-local cbc_shared = "/home/shared/cbc"
-local cbc_software = cbc_shared .. "/software"
-
-prepend_path("PATH", cbc_software .. "/" .. name .. "-" .. version .. "/bin")
+whatis("Description: The GDC Data Transfer Tool Client provides a command-line interface supporting both GDC data downloads and submissions. Example: `gdc-client --version`.")
 
 load("python/2.7.10")
 unload("anaconda-python")
+
+local path = "/opt/GDC"
+local home = path .. "/" .. name .. "-" .. version
+prepend_path("PATH",  home .. "/bin")
