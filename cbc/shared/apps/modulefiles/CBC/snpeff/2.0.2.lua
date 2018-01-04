db996ee6aefb80d47f790af753d6d7f9af6ac4f0
hb <hb@aroma-project.org>
2018-01-04 09:59:04 -0800

    MODULES: Now 'snpeff' module points directly to installation path

--- cbc/shared/apps/modulefiles/CBC/snpeff/2.0.2.lua
+++ cbc/shared/apps/modulefiles/CBC/snpeff/2.0.2.lua
@@ -1,20 +1,21 @@
 help([[
-SnpEff - Genetic Variant Annotation and Effect Prediction Toolbox
+SnpEff: Genetic Variant Annotation and Effect Prediction Toolbox
 ]])
 
-local name = "snpEff"
-local version = "2.0.2"
+local name = myModuleName()
+local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: high-throughput sequencing")
 whatis("URL: http://snpeff.sourceforge.net/")
-whatis("Description: SnpEff is a variant annotation and effect prediction tool. It annotates and predicts the effects of variants on genes (such as amino acid changes).")
+whatis("Description: SnpEff is a variant annotation and effect prediction tool. It annotates and predicts the effects of variants on genes (such as amino acid changes). Example: `java -jar $SNPEFF -help`")
 
--- Local variables
-local cbc_shared = "/home/shared/cbc"
-local cbc_software = cbc_shared .. "/software"
+load("jdk")
 
-local path = cbc_software .. "/" .. name .. "-" .. version
-local pathname = path .. "/snpEff.jar"
-
-set_alias("snpEff", "java -jar " .. pathname)
+local path = "/home/jocostello/tools"
+local name = "snpEff"
+local version = "2_0_2"
+local home = path .. "/" .. name .. "_v" .. version
+local jarfile = home .. "/snpEff.jar"
+setenv("SNPEFF", jarfile)
+set_alias("snpEff", "java -jar " .. jarfile)
 
