c0d9058ea8ba0f4a30e7c4acdd8e37718e86935d
hb <hb@aroma-project.org>
2018-01-04 09:30:57 -0800

    MODULES: Now 'cufflinks' module points directly to system installation folder

--- cbc/shared/apps/modulefiles/CBC/cufflinks/1.0.3.lua
+++ cbc/shared/apps/modulefiles/CBC/cufflinks/1.0.3.lua
@@ -1,16 +1,14 @@
 help([[
-Cufflinks: Transcriptome assembly and differential expression analysis for RNA-Seq
+Cufflinks: Transcriptome Assembly and Differential Expression Analysis for RNA-Seq
 ]])
 
 local name = myModuleName()
 local version = myModuleVersion()
 whatis("Version: " .. version)
-whatis("Keywords: ")
+whatis("Keywords: sequencing")
 whatis("URL: http://cole-trapnell-lab.github.io/cufflinks/")
-whatis("Description: Cufflinks assembles transcripts, estimates their abundances, and tests for differential expression and regulation in RNA-Seq samples. It accepts aligned RNA-Seq reads and assembles the alignments into a parsimonious set of transcripts. Cufflinks then estimates the relative abundances of these transcripts based on how many reads support each one, taking into account biases in library preparation protocols.")
+whatis("Description: Cufflinks assembles transcripts, estimates their abundances, and tests for differential expression and regulation in RNA-Seq samples. It accepts aligned RNA-Seq reads and assembles the alignments into a parsimonious set of transcripts. Cufflinks then estimates the relative abundances of these transcripts based on how many reads support each one, taking into account biases in library preparation protocols. Example: `cufflinks`.")
 
--- Local variables
-local cbc_shared = "/home/shared/cbc"
-local cbc_software = cbc_shared .. "/software"
-
-prepend_path("PATH", cbc_software .. "/" .. name .. "-" .. version .. ".Linux_x86_64")
+local path = "/opt/local/bin"
+local home = path .. "/" .. name .. "-" .. version .. ".Linux_x86_64"
+prepend_path("PATH", home)
