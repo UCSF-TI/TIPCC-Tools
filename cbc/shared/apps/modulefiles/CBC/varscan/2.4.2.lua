cb17b033057a87712f40ddfd1d79fc362695eb2f
hb <hb@aroma-project.org>
2018-01-04 14:34:36 -0800

    Forgot to add file

---
+++ cbc/shared/apps/modulefiles/CBC/varscan/2.4.2.lua
@@ -0,0 +1,17 @@
+help([[
+VarScan: Variant Detection in Massively Parallel Sequencing Data
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: high-throughput sequencing")
+whatis("URL: https://dkoboldt.github.io/varscan/")
+whatis("Description: VarScan is a platform-independent mutation caller for targeted, exome, and whole-genome resequencing data generated on Illumina, SOLiD, Life/PGM, Roche/454, and similar instruments. Example: `java -jar $VARSCAN`.")
+
+local path = "/home/shared/cbc/software_cbc"
+name = "VarScan"
+local home = path .. "/" .. name .. "-" .. version
+local jarfile = home .. "/" .. name .. ".v" .. version .. ".jar"
+setenv("VARSCAN", jarfile)
+set_alias("varscan", "java -jar " .. jarfile)
