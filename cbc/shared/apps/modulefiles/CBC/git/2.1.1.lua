2ca7da0e2b03c63f09f81b76cdd9f9b5b2ba11c8
hb <hb@aroma-project.org>
2018-01-04 09:51:43 -0800

    MODULES: Now 'git' module points directly to system installation folder

--- cbc/shared/apps/modulefiles/CBC/git/2.1.1.lua
+++ cbc/shared/apps/modulefiles/CBC/git/2.1.1.lua
@@ -1,5 +1,5 @@
 help([[
-Git - Distributed Version Control System
+Git: Distributed Version Control System
 ]])
 
 local name = myModuleName()
@@ -7,10 +7,8 @@ local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: version control")
 whatis("URL: https://git-scm.com/")
-whatis("Description: Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.")
+whatis("Description: Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency. Example: `git --version`.")
 
--- Local variables
-local cbc_shared = "/home/shared/cbc"
-local cbc_software = cbc_shared .. "/software"
-
-prepend_path("PATH", cbc_software .. "/" .. name .. "-" .. version)
+local path = "/home/shared/cbc/software_cbc"
+local home = path .. "/" .. name .. "-" .. version
+prepend_path("PATH", home)
