a2ad88e414bee6e2ffd2cff02899184731d85aa4
hb <hb@aroma-project.org>
2018-01-04 17:39:04 -0800

    MODULES: Tweak example

--- cbc/shared/apps/modulefiles/CBC/blast/2.2.26.lua
+++ cbc/shared/apps/modulefiles/CBC/blast/2.2.26.lua
@@ -7,7 +7,7 @@ local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: programming, scripting")
 whatis("URL: https://blast.ncbi.nlm.nih.gov/Blast.cgi")
-whatis("Description: BLAST finds regions of similarity between biological sequences. The program compares nucleotide or protein	sequences to sequence databases and calculates the statistical significance.")
+whatis("Description: BLAST finds regions of similarity between biological sequences. The program compares nucleotide or protein	sequences to sequence databases and calculates the statistical significance. Example: `blastx -version`")
 
 local path = "/opt/local/bin"
 local home = path .. "/" .. name .. "-" .. version
