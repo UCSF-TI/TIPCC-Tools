6fb287998f2fd017a7da398b8213129c3ba919c5
hb <hb@aroma-project.org>
2018-01-17 19:47:33 -0800

    MODULES: Add 'ack'

---
+++ cbc/shared/apps/modulefiles/CBC/ack/2.22.lua
@@ -0,0 +1,14 @@
+help([[
+ack: A Tool Like Grep, Optimized for Programmers
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: ack, ag, git-grep, grep, rg")
+whatis("URL: https://beyondgrep.com/")
+whatis("Description: Designed for programmers with large heterogeneous trees of source code, 'ack' is written purely in portable Perl 5 and takes advantage of the power of Perl's regular expressions. Examples: `ack --version` and `ack --man`")
+
+local path = "/home/shared/cbc/software_cbc"
+local home = path .. "/" .. name .. "-" .. version
+prepend_path("PATH",  home)
