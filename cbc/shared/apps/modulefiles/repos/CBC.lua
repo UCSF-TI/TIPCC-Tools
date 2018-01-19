d97d1e55ccb91c8abae8990bf23807c48eee4084
hb <hb@aroma-project.org>
2018-01-19 10:46:17 -0800

    module load CBC: now setting SOFTWARE_ROOT_CBC

--- cbc/shared/apps/modulefiles/repos/CBC.lua
+++ cbc/shared/apps/modulefiles/repos/CBC.lua
@@ -11,3 +11,4 @@ whatis("URL: http://cbi.ucsf.edu/")
 whatis("Description: Repository of modules shared by Computational Biology and Informatics (http://cbi.ucsf.edu/) which was named Computational Biology Core (CBC) until 2017. When loading this module, you will get access to a large number of software modules.  When unloading the module, any software modules from this " .. name .. " repository still loaded when you unload this repository module, will remain in your list of loaded modules but will be inactivate (i.e. behave as they never were loaded) until you reload this repository module again.  Example: `module load CBC` and then `module avail`.")
 
 prepend_path("MODULEPATH", mpath)
+setenv("SOFTWARE_ROOT_CBC", "/home/shared/cbc/software_cbc")
