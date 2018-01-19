1d3c7476a867c8cc0052083803a9ffa90b27de7f
hb <hb@aroma-project.org>
2018-01-19 12:18:55 -0800

    MODULES: igv modules were not automatically built

--- cbc/shared/apps/modulefiles/CBC/igv/2.3.31.lua
+++ cbc/shared/apps/modulefiles/CBC/igv/2.3.31.lua
@@ -8,10 +8,9 @@ local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: ")
 whatis("URL: https://software.broadinstitute.org/software/igv/")
-whatis("Description: The Integrative Genomics Viewer (IGV) is a high-performance visualization tool for interactive exploration of large, integrated genomic datasets. It supports a wide variety of data types, including array-based and next-generation sequence data, and genomic annotations.")
+whatis("Description: The Integrative Genomics Viewer (IGV) is a high-performance visualization tool for interactive exploration of large, integrated genomic datasets. It supports a wide variety of data types, including array-based and next-generation sequence data, and genomic annotations. Example: `igv`.")
 
--- Local variables
-local path = "/home/shared/cbc/software_cbc"
+local path = os.getenv("SOFTWARE_ROOT_CBC")
 local home = path .. "/" .. name .. "_" .. version
 prepend_path("PATH", home)
 set_alias("igv", home .. "/igv.sh")
