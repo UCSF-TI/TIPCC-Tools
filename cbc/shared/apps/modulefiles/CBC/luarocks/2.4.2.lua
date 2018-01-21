9a37def60596eaeb147d81310135fd1724657c84
hb <hb@aroma-project.org>
2018-01-21 13:05:46 -0800

    MODULES: Add example to 'luarocks'

--- cbc/shared/apps/modulefiles/CBC/luarocks/2.4.2.lua
+++ cbc/shared/apps/modulefiles/CBC/luarocks/2.4.2.lua
@@ -7,7 +7,7 @@ local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: programming, scripting")
 whatis("URL: https://luarocks.org/")
-whatis("Description: LuaRocks is the package manager for Lua modules.")
+whatis("Description: LuaRocks is the package manager for Lua modules. Example: `luarocks --help`.")
 
 local path = os.getenv("SOFTWARE_ROOT_CBC")
 local home = path .. "/" .. name .. "-" .. version
