9d9b053ab94685d9649228afc416c108596c78ca
hb <hb@aroma-project.org>
2018-01-22 20:19:14 -0800

    MODULES: Add git/2.16.1

---
+++ cbc/shared/apps/modulefiles/CBC/git/2.16.1.lua
@@ -0,0 +1,17 @@
+help([[
+Git: Distributed Version Control System
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: version control")
+whatis("URL: https://git-scm.com/")
+whatis("Description: Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency. Example: `git --version`.")
+
+local path = os.getenv("SOFTWARE_ROOT_CBC")
+local home = path .. "/" .. name .. "-" .. version
+prepend_path("PATH", home .. "/bin")
+
+-- Unset alias 'git', iff set
+set_alias("git")
