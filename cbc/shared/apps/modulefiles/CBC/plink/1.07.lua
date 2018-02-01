6333b49bd078eca1af6551eac9ee91eb135dae06
hb <hb@aroma-project.org>
2018-02-01 15:28:30 -0800

    MODULES: Add 'plink'

---
+++ cbc/shared/apps/modulefiles/CBC/plink/1.07.lua
@@ -0,0 +1,14 @@
+help([[
+PLINK: Whole Genome Association Analysis Toolset
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: genomics")
+whatis("URL: http://zzz.bwh.harvard.edu/plink/index.shtml")
+whatis("Description: PLINK is a free, open-source whole genome association analysis toolset, designed to perform a range of basic, large-scale analyses in a computationally efficient manner. The focus of PLINK is purely on analysis of genotype/phenotype data, so there is no support for steps prior to this (e.g. study design and planning, generating genotype or CNV calls from raw data). Example: `plink --help`.")
+local path = os.getenv("SOFTWARE_ROOT_CBC")
+local home = path .. "/" .. name .. "-" .. version
+prepend_path("PATH", home)
+
