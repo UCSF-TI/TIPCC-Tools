54427da4eb2051a3a67927a66b7143a798d5d228
hb <hb@aroma-project.org>
2018-02-01 23:03:00 -0800

    MODULES: Add 'xz/5.2.3'

---
+++ cbc/shared/apps/modulefiles/CBC/xz/5.2.3.lua
@@ -0,0 +1,19 @@
+help([[
+XZ Utils: General-purpose Data Compression Software
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: library, compression")
+whatis("URL: http://libevent.org/, https://github.com/libevent/libevent")
+whatis("Description: XZ Utils is free general-purpose data compression software with a high compression ratio. XZ Utils were written for POSIX-like systems, but also work on some not-so-POSIX systems. XZ Utils are the successor to LZMA Utils. The core of the XZ Utils compression code is based on LZMA SDK, but it has been modified quite a lot to be suitable for XZ Utils. The primary compression algorithm is currently LZMA2, which is used inside the .xz container format. With typical files, XZ Utils create 30 % smaller output than gzip and 15 % smaller output than bzip2. Example: `xz --version`.")
+
+local path = os.getenv("SOFTWARE_ROOT_CBC")
+local home = path .. "/" .. name .. "-" .. version
+prepend_path("PATH", home .. "/bin")
+prepend_path("MANPATH", home .. "/share/man")
+prepend_path("LIBRARY_PATH", home .. "/lib")
+prepend_path("PKG_CONFIG_PATH", home .. "/lib/pkgconfig")
+prepend_path("LD_LIBRARY_PATH", home .. "/lib")
+prepend_path("CPATH", home .. "/include")
