5ed96bac1023b9fc38085b6499e36e41c26c68a2
hb <hb@aroma-project.org>
2018-03-06 18:27:43 -0800

    MODULES: updated asciigenome and bedtools

---
+++ cbc/shared/apps/modulefiles/CBC/bedtools2/2.27.1.lua
@@ -0,0 +1,17 @@
+help([[
+bedtools2: The Swiss Army Knife for Genome Arithmetic
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: sequencing")
+whatis("URL: https://github.com/arq5x/bedtools2/")
+whatis("Description: Collectively, the bedtools utilities are a swiss-army knife of tools for a wide-range of genomics analysis tasks. The most widely-used tools enable genome arithmetic: that is, set theory on the genome. For example, bedtools allows one to intersect, merge, count, complement, and shuffle genomic intervals from multiple files in widely-used genomic file formats such as BAM, BED, GFF/GTF, VCF. Example: `bedtools --version`.")
+
+load("gcc")
+load("zlib")
+
+local path = os.getenv("SOFTWARE_ROOT_CBC")
+local home = path .. "/" .. name .. "-" .. version
+prepend_path("PATH", home .. "/bin")
