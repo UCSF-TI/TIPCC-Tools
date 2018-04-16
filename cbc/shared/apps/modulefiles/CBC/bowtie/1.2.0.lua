3d8f8fb4973e033ec8b2971a3169555d7b593ea3
hb <hb@aroma-project.org>
2018-04-16 12:29:15 -0700

    MODULE: 'bowtie' needs a modern Python

--- cbc/shared/apps/modulefiles/CBC/bowtie/1.2.0.lua
+++ cbc/shared/apps/modulefiles/CBC/bowtie/1.2.0.lua
@@ -9,6 +9,8 @@ whatis("Keywords: sequencing")
 whatis("URL: http://bowtie-bio.sourceforge.net/index.shtml")
 whatis("Description: Bowtie is an ultrafast, memory-efficient short read aligner. _NOTE: This is Bowtie v1 - not v2_.  Example: `bowtie --version`")
 
+load("python")
+
 local path = os.getenv("SOFTWARE_ROOT_CBC")
 local home = path .. "/" .. name .. "-" .. version
 prepend_path("PATH", home .. "/bin")
