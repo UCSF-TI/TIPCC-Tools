e5e245590e1d1d69689cb35ed05ce12c6971161d
hb <hb@aroma-project.org>
2018-04-19 15:33:18 -0700

    MODULES: Add htop/2.2.0

--- cbc/shared/apps/modulefiles/CBC/r/3.5.0rc.lua
+++ cbc/shared/apps/modulefiles/CBC/r/3.5.0rc.lua
@@ -9,7 +9,7 @@ whatis("Keywords: Programming, Statistics")
 whatis("URL: https://www.r-project.org/")
 whatis("Description: The R programming language. Example: `R --version` and `Rscript --version`.")
 
-depends_on("gcc/4.9.2", "netcdf/4.3.0", "bzip2/1.0.6", "pcre/8.42")
+depends_on("gcc/4.9.2", "netcdf", "bzip2", "pcre", "xz", "libpng", "curl", "zlib")
 
 local path = os.getenv("SOFTWARE_ROOT_CBC")
 local home = path .. "/R/R-" .. version .. "-20180416"
