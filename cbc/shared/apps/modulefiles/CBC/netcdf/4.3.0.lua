8dcf697d08524599586dfcad7e35e383f4d538c6
hb <hb@aroma-project.org>
2018-04-19 11:23:53 -0700

    MODULES: Add 'netcdf/4.3.0'

---
+++ cbc/shared/apps/modulefiles/CBC/netcdf/4.3.0.lua
@@ -0,0 +1,19 @@
+help([[
+NetCDF: Network Common Data Form
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: programming, library")
+whatis("URL: https://www.unidata.ucar.edu/software/netcdf/, https://github.com/Unidata/netcdf-c")
+whatis("Description: NetCDF is a set of software libraries and self-describing, machine-independent data formats that support the creation, access, and sharing of array-oriented scientific data. Example: `nc-config --version` and `ncdump --version`.")
+
+local path = "/opt/NetCDF"
+local home = path .. "/NetCDF-" .. version
+prepend_path("PATH", home .. "/bin")
+prepend_path("MANPATH", home .. "/share/man")
+prepend_path("LIBRARY_PATH", home .. "/lib")
+prepend_path("PKG_CONFIG_PATH", home .. "/lib/pkgconfig")
+prepend_path("LD_LIBRARY_PATH", home .. "/lib")
+prepend_path("CPATH", home .. "/include")
