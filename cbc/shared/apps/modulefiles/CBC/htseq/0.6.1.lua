813caaa6f707c3de4e07fdff67af96b0f1ec1b48
hb <hb@aroma-project.org>
2018-04-19 13:05:54 -0700

    BUG FIX: Module 'htseq' depends on the deprecated 'anaconda' module

--- cbc/shared/apps/modulefiles/CBC/htseq/0.6.1.lua
+++ cbc/shared/apps/modulefiles/CBC/htseq/0.6.1.lua
@@ -9,6 +9,8 @@ whatis("Keywords: programming, scripting")
 whatis("URL: http://www-huber.embl.de/users/anders/HTSeq/")
 whatis("Description: HTSeq is a Python package that provides infrastructure to process data from high-throughput sequencing assays. Example: `htseq-count` and `htseq-qa`.")
 
+depends_on("anaconda")
+
 local path = os.getenv("SOFTWARE_ROOT_CBC")
 local home = path .. "/" .. name .. "-" .. version
 prepend_path("PATH", home .. "/scripts")
