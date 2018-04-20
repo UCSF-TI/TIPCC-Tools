671707b550f6fa53e2e52dd0b7d2df8011b4f61b
hb <hb@aroma-project.org>
2018-04-19 18:05:35 -0700

    BUG FIX: module 'libpng/1.6.34' did not set PATH and also not CFLAGS and LDFLAGS

--- cbc/shared/apps/modulefiles/CBC/libpng/1.6.34.lua
+++ cbc/shared/apps/modulefiles/CBC/libpng/1.6.34.lua
@@ -7,12 +7,21 @@ local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: library, PNG")
 whatis("URL: http://www.libpng.org/pub/png/libpng.html")
-whatis("Description: libpng is the official PNG reference library. It supports almost all PNG features, is extensible, and has been extensively tested for over 22 years.  Example: `libpng16-config --version`.")
+whatis("Description: libpng is the official PNG reference library. It supports almost all PNG features, is extensible, and has been extensively tested for over 22 years.  Example: `libpng-config --version`.")
 
 local path = os.getenv("SOFTWARE_ROOT_CBC")
 local home = path .. "/" .. name .. "-" .. version
+
+prepend_path("PATH", home .. "/bin")
 prepend_path("MANPATH", home .. "/share/man")
-prepend_path("LIBRARY_PATH", home .. "/lib")
-prepend_path("PKG_CONFIG_PATH", home .. "/lib/pkgconfig")
 prepend_path("LD_LIBRARY_PATH", home .. "/lib")
+
+prepend_path("PKG_CONFIG_PATH", home .. "/lib/pkgconfig")
+
+-- used by ./configure
+prepend_path("CFLAGS", "-I" .. home .. "/include", " ")
+prepend_path("LDFLAGS", "-L" .. home .. "/lib", " ")
+
+-- used by GCC compiler/linker
 prepend_path("CPATH", home .. "/include")
+prepend_path("LIBRARY_PATH", home .. "/lib")
