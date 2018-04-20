49465a16b0119c087383c9ab3a04cb848b816088
hb <hb@aroma-project.org>
2018-04-20 12:54:05 -0700

    fix

--- cbc/shared/apps/modulefiles/CBC/cbc-bin/0.1.2.lua
+++ cbc/shared/apps/modulefiles/CBC/cbc-bin/0.1.2.lua
@@ -9,7 +9,6 @@ whatis("Keywords: UCSF, cluster, utilities")
 whatis("URL: http://cbi.ucsf.edu/")
 whatis("Description: Utility scripts and binaries shared by UCSF CBI (formerly CBC).")
 
-local cbc_shared = "/home/shared/cbc/shared"
-prepend_path("PATH", cbc_shared .. "/bin")
-prepend_path("PATH", cbc_shared .. "/local/bin")
+prepend_path("PATH", "/home/shared/cbc/shared/bin")
+prepend_path("PATH", "/home/shared/cbc/local/bin")
 
