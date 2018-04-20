bcdd23ffc28684d4facd828f067aeb8b30315914
hb <hb@aroma-project.org>
2018-04-19 18:13:31 -0700

    MODULES: Add r/3.4.4patched

---
+++ cbc/shared/apps/modulefiles/CBC/r/3.4.4patched.lua
@@ -0,0 +1,15 @@
+help([[
+R: The R Programming Language
+]])
+
+local name = myModuleName()
+local version = myModuleVersion()
+whatis("Version: " .. version)
+whatis("Keywords: Programming, Statistics")
+whatis("URL: https://www.r-project.org/")
+whatis("Description: The R programming language. Example: `R --version` and `Rscript --version`.")
+
+local path = os.getenv("SOFTWARE_ROOT_CBC")
+local home = path .. "/R/R-3.4.4patched-20180419"
+prepend_path("PATH", home .. "/bin")
+prepend_path("MANPATH", home .. "/share/man")
