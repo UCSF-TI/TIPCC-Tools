9f7e0c11be422e650f1cf4d888a1010460dee982
hb <hb@aroma-project.org>
2018-04-23 09:59:11 -0700

    MODULES: Add r/3.5.0

--- cbc/shared/apps/modulefiles/CBC/r/3.5.0.lua
+++ cbc/shared/apps/modulefiles/CBC/r/3.5.0.lua
@@ -3,15 +3,14 @@ R: The R Programming Language
 ]])
 
 local name = myModuleName()
+name = "R"
 local version = myModuleVersion()
 whatis("Version: " .. version)
 whatis("Keywords: Programming, Statistics")
 whatis("URL: https://www.r-project.org/")
 whatis("Description: The R programming language. Example: `R --version` and `Rscript --version`.")
 
-depends_on("gcc/4.9.2", "netcdf", "bzip2", "pcre", "xz", "libpng", "curl", "zlib")
-
 local path = os.getenv("SOFTWARE_ROOT_CBC")
-local home = path .. "/R/R-" .. version .. "-20180416"
+local home = path .. "/R/" .. name .. "-" .. version
 prepend_path("PATH", home .. "/bin")
 prepend_path("MANPATH", home .. "/share/man")
