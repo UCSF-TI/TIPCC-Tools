4591e72689f482751e91d08711dac2f7004862b6
hb <hb@aroma-project.org>
2018-04-23 13:33:01 -0700

    Fix; forgot to specify dependencies for 'r/3.5.0'

--- cbc/shared/apps/modulefiles/CBC/r/3.5.0.lua
+++ cbc/shared/apps/modulefiles/CBC/r/3.5.0.lua
@@ -10,6 +10,8 @@ whatis("Keywords: Programming, Statistics")
 whatis("URL: https://www.r-project.org/")
 whatis("Description: The R programming language. Example: `R --version` and `Rscript --version`.")
 
+depends_on("gcc/4.9.2", "netcdf", "bzip2", "pcre", "xz", "libpng", "curl", "zlib")
+
 local path = os.getenv("SOFTWARE_ROOT_CBC")
 local home = path .. "/R/" .. name .. "-" .. version
 prepend_path("PATH", home .. "/bin")
