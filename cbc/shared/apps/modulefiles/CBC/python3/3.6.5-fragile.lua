801a5509b7c1da87e352f9cf947c061c2b973015
hb <hb@aroma-project.org>
2018-06-01 23:58:37 -0700

    Warn that python3/3.6.5 is fragile

--- cbc/shared/apps/modulefiles/CBC/python3/3.6.5-fragile.lua
+++ cbc/shared/apps/modulefiles/CBC/python3/3.6.5-fragile.lua
@@ -11,7 +11,8 @@ whatis("Keywords: Programming")
 whatis("URL: https://www.python.org/")
 whatis("Description: Python is a programming language that lets you work quickly and integrate systems more effectively.  Example: `python3 --version` as well as `virtualenv myproject`, `. myproject/bin/activate`, and `pip install cnvkit`.")
 
-depends_on("openssl", "libpng", "bzip2", "pcre", "xz", "zlib")
+-- "openssl" not available
+depends_on("libpng", "bzip2", "pcre", "xz", "zlib")
 
 local path = os.getenv("SOFTWARE_ROOT_CBC")
 local home = path .. "/" .. name .. "-" .. version
@@ -22,3 +23,5 @@ prepend_path("MANPATH", home .. "/share/man")
 
 -- No need to set PYTHONPATH; taken care of automatically
 -- prepend_path("PYTHONPATH", home .. "/lib/python3.6")
+
+io.stderr:write("WARNING: The python3/3.6.5 module is fragile - the build does NOT pass all tests. Use at your own risk!\n")
