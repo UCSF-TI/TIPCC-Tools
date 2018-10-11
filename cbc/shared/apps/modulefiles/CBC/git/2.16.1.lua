4ecb04c530140a6609f46a9e5eebab060ba71c8f
hb <hb@aroma-project.org>
2018-10-11 14:11:00 -0700

    MODULES: Module 'git' now depends on module 'curl' (because system-install curl is too old resulting in 'SSL connect error')

--- cbc/shared/apps/modulefiles/CBC/git/2.16.1.lua
+++ cbc/shared/apps/modulefiles/CBC/git/2.16.1.lua
@@ -9,6 +9,8 @@ whatis("Keywords: version control")
 whatis("URL: https://git-scm.com/")
 whatis("Description: Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency. Example: `git --version`.")
 
+depends_on("curl")
+
 local path = os.getenv("SOFTWARE_ROOT_CBC")
 local home = path .. "/" .. name .. "-" .. version
 prepend_path("PATH", home .. "/bin")
