a77d6dfbd8a8af9f78ffadfe1cfe1e0176fab8f6
hb <hb@aroma-project.org>
2018-12-07 18:25:34 -0800

    MODULES: 'byobu' requires 'tmux'

--- cbc/shared/apps/modulefiles/CBC/byobu/5.124.lua
+++ cbc/shared/apps/modulefiles/CBC/byobu/5.124.lua
@@ -9,6 +9,8 @@ whatis("Keywords: terminal, SSHH")
 whatis("URL: http://byobu.org, https://github.com/dustinkirkland/byobu")
 whatis("Description: Byobu is an elegant enhancement of the otherwise functional, plain, practical GNU Screen. Byobu includes an enhanced profile, configuration utilities, and system status notifications for the GNU screen window manager as well as the Tmux terminal multiplexer. Example: `byobu --version`")
 
+depends_on("tmux")
+
 local path = os.getenv("SOFTWARE_ROOT_CBC")
 local home = path .. "/" .. name .. "-" .. version
 prepend_path("PATH",  home .. "/bin")
